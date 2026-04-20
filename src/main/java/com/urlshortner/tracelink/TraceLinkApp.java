d91e63c57ac21dceb6ff9cde77cc4e884f92004a
BenGJ10 <bengj1015@gmail.com>
2026-04-20 20:49:12 +0530

    feat: initialized spring-boot

---
+++ src/main/java/com/urlshortner/tracelink/TraceLinkApp.java
@@ -0,0 +1,13 @@
+package com.urlshortner.tracelink;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class TraceLinkApp{
+
+    public static void main(String[] args) {
+        SpringApplication.run(TraceLinkApp.class, args);
+    }
+
+}
