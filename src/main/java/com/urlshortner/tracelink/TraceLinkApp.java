9f81f5cc96a4c508ad5921470385bdb80bb2e7bb
BenGJ10 <bengj1015@gmail.com>
2026-05-01 16:35:29 +0530

    feat: updated configurations

--- src/main/java/com/urlshortner/tracelink/TraceLinkApp.java
+++ src/main/java/com/urlshortner/tracelink/TraceLinkApp.java
@@ -2,8 +2,10 @@ package com.urlshortner.tracelink;
 
 import org.springframework.boot.SpringApplication;
 import org.springframework.boot.autoconfigure.SpringBootApplication;
+import org.springframework.scheduling.annotation.EnableAsync;
 
 @SpringBootApplication
+@EnableAsync
 public class TraceLinkApp{
 
     public static void main(String[] args) {
